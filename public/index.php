2a5da9920c0b3a5b9d7d66ed631d6e8253ad6c5f
Albert Gabbasov <albertxgm@gmail.com>
2021-12-05 02:10:09 +0300

    Урок 5. Класс обработки ошибок

--- public/index.php
+++ public/index.php
@@ -4,4 +4,5 @@ require_once dirname(__DIR__) . "/config/init.php";
 require_once LIBS . '/functions.php';
 
 new \shop\App();
-debug(\shop\App::$app->getProperties());
+
+throw new Exception('Страница не найдена', 404);
