43c9f32546087bf72f0f4e41fde4571be3eccc59
Albert Gabbasov <albertxgm@gmail.com>
2021-12-06 03:09:45 +0300

    Урок 6. Класс Маршрутизатор. Часть 1

---
+++ vendor/shop/core/Router.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace shop;
+
+class Router
+{
+    protected static $routes = [];
+    protected static $route = [];
+
+    public static function add($regexp, $route = [])
+    {
+        self::$routes[$regexp] = $route;
+    }
+
+    public static function getRoutes()
+    {
+        return self::$routes;
+    }
+
+    public static function dispatch($url)
+    {
+        if (self::matchRoute($url)) {
+            echo 'OK';
+        } else {
+            echo 'NO';
+        }
+    }
+
+    public static function matchRoute($url)
+    {
+        return true;
+    }
+}
